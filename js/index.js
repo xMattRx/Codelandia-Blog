4b5abb6a1b05d3434631da0f3a91bb9096038aec
Matthew Rachid Romie Nascimento <matthew.nascimento@compasso.com.br>
2022-07-07 09:51:34 -0300

    challenge ready

---
+++ js/index.js
@@ -0,0 +1,11 @@
+  const sr = ScrollReveal({
+      origin: 'top',
+      distance: '50px',
+      duration: 2000,
+      reset: false
+  });
+
+  ScrollReveal().reveal('.container-main-item', { delay: 100 });
+  ScrollReveal().reveal('.container-main-item-header', { delay: 300 });
+  ScrollReveal().reveal('.container-main-item-body-title', { delay: 400 });
+  ScrollReveal().reveal('.container-main-item-body-description', { delay: 500 });
